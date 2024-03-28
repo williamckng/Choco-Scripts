a4dd21889dcbad6e7c8721a3afe7646c6d3de428
William Ng <william@williamng.info>
2024-03-28 21:34:18 +0800

    Commit Foxit Reader Scripts

---
+++ FoxitReader/tools/chocolateyInstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Localize Chocolatey environment variables
+$chocolateyPackageVersion = $env:chocolateyPackageVersion
+$chocolateyPackageName = $env:chocolateyPackageName
+
+# Define other local variables
+$softwareName = 'Foxit PDF Reader'
+$fileName = 'FoxitPDFReader20241_L10N_Setup.msi'
+$fileLocation = '\\192.168.100.1\DS-Create$\Applications'
+$msiParams = "TRANSFORMS=`"$fileLocation\$softwareName\FoxitPDFReader20241_L10N_Setup_FCT.mst`""
+
+# Define source location
+$file = "$fileLocation\$softwareName\$fileName"
+
+# Define install parameters
+$installLog = "$env:TEMP\$chocolateyPackageName.$chocolateyPackageVersion.MsiInstall.log"
+$silentArgs = "/passive /norestart /l*v `"$installLog`" $msiParams"
+
+
+# Define Chocolatey parameter hash table
+$packageArgs = @{
+  packageName    = $chocolateyPackageName
+  fileType       = 'MSI'
+  file           = $file
+  softwareName   = $softwareName
+  silentArgs     = $silentArgs
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
