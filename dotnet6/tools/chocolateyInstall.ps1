aeb8ae9f4f2b50561555dbc208e8846d65eee269
William Ng <william@williamng.info>
2024-03-29 09:53:43 +0800

    Commited dotnet6 installation scripts

---
+++ dotnet6/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Localize Chocolatey environment variables
+$chocolateyPackageVersion = $env:chocolateyPackageVersion
+$chocolateyPackageName = $env:chocolateyPackageName
+
+# Define other local variables
+$softwareName = 'Microsoft Windows Desktop Runtime'
+$fileName = "windowsdesktop-runtime-$chocolateyPackageVersion-win-x64.exe"
+$fileLocation = '\\192.168.100.1\DS-Create$\Applications'
+
+# Define source location
+$file = "$fileLocation\$softwareName\$fileName"
+
+# Define install parameters
+$installLog = "$env:TEMP\$chocolateyPackageName.$chocolateyPackageVersion.MsiInstall.log"
+$silentArgs = "/install /passive /norestart /log $installLog"
+
+# Define Chocolatey parameter hash table
+$packageArgs = @{
+  packageName    = $ChocolateyPackageName
+  fileType       = 'exe'
+  file           = $file
+  softwareName   = $softwareName
+  silentArgs     = $silentArgs
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
