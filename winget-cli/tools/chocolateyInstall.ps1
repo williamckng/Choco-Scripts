00ca639048324862b07437dcef2c632609d32f17
William Ng <william@williamng.info>
2024-03-29 11:48:32 +0800

    Update winget to 1.7.10861

---
+++ winget-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'Microsoft Winget'
+$fileName     = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+$fileLocation = "\\192.168.100.1\DS-Create$\Applications\$softwareName"
+$licenseName  = '30fe89a9836a4cfbbd3fedce72a58680_License1.xml'
+
+Add-AppxProvisionedPackage -Online -PackagePath "$fileLocation\$fileName" -LicensePath "$fileLocation\$licenseName" -Regions All
